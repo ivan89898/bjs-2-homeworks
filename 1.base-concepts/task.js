e68e28057bc811419777280147a6a94d9b228c97
ivan89898 <chizhevskiy.ivan@mail.ru>
2023-03-16 18:32:30 +0500

    Первое задание готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,37 @@
-"use strict"
+`use strict`;
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+  let result = [];
+
+  d = b ** 2 - 4 * a * c;
+  switch (true) {
+    case d === 0:
+      result = [-b / (2 * a)];
+      break;
+
+    case d > 0:
+      result = [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
+      break;
+  }
+  return result;
 }
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+function calculateTotalMortgage(percent, contribution, amount, date) {
+  if (isNaN(percent) === true) {
+    return false;
+  }
+  if (isNaN(amount) === true) {
+    return false;
+  }
+  if (isNaN(contribution) === true) {
+    return false;
+  }
+
+  S = amount - contribution;
+  P = (1 / 12) * percent;
+  n = date;
+  totalAmount = parseFloat((S * (P + P / (((1 + P) ^ n) - 1))).toFixed(2));
+
+  // код для задачи №2 писать здесь
+
+  return totalAmount;
 }
