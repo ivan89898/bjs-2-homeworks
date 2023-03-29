17269df2c23826957870a7de396dff97810d9a27
ivan89898 <chizhevskiy.ivan@mail.ru>
2023-03-30 01:46:37 +0500

    Initial commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,19 @@
 function compareArrays(arr1, arr2) {
-  
+  const result =
+    arr1.length === arr2.length &&
+    arr1.every((value, index) => value === arr2[index]);
+  return result;
 }
+function getUsersNamesInAgeRange(arr, gender) {
+  let result =
+    arr
+      .filter(() => gender === "мужской")
+      .map((item) => item.age)
+      .reduce((a, b) => a + b / arr.length, 0) ||
+    arr
+      .filter(() => gender === "женский")
+      .map((item) => item.age)
+      .reduce((a, b) => a + b / arr.length, 0);
 
-function getUsersNamesInAgeRange(users, gender) {
-  
+  return result; // array
 }
