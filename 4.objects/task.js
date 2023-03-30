77f7ba9bfa650cd94afb6e7a29e4ddd558961545
ivan89898 <chizhevskiy.ivan@mail.ru>
2023-03-31 00:32:05 +0500

    Initial commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,35 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
+let student1 = new Student("Ivan", "male", 28);
+let student2 = new Student("Jhon", "male", 29);
+let student3 = new Student("Dasha", "women", 23);
+let student4 = new Student("Sonya", "women", 35);
 Student.prototype.setSubject = function (subjectName) {
-  
-}
+  this.subject = subjectName;
+};
 
-Student.prototype.addMarks = function (...marks) {
-  
-}
+Student.prototype.addMarks = function addMarks(...marksToAdd) {
+  if (this.marks === undefined) {
+    this.marks === [...marksToAdd];
+  } else {
+    this.marks.push(...marksToAdd);
+  }
+};
 
 Student.prototype.getAverage = function () {
-  
-}
+  if (this.marks === undefined) {
+    return 0;
+  }
+  return this.marks.reduce((a, b) => a + b / this.marks.length, 0);
+};
 
-Student.prototype.exclude = function (reason) {
-  
-}
+Student.prototype.exclude = function exclude(reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+};
